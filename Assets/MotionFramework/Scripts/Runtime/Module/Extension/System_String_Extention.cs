fc82a9afb5a6c47a6a67357ff5219d88371ae498
何冠峰 <hevinci@hotmail.com>
2020-06-14 14:36:30 +0800

    Update extension
    Add system.string extension

---
+++ Assets/MotionFramework/Scripts/Runtime/Module/Extension/System_String_Extention.cs
@@ -0,0 +1,27 @@
+﻿//--------------------------------------------------
+// Motion Framework
+// Copyright©2018-2020 何冠峰
+// Licensed under the MIT license
+//--------------------------------------------------
+
+namespace System
+{
+	public static class System_String_Extention
+	{
+		/// <summary>
+		/// 移除首个字符
+		/// </summary>
+		public static string RemoveFirstChar(this System.String o)
+		{
+			return o.Substring(1);
+		}
+
+		/// <summary>
+		/// 移除末尾字符
+		/// </summary>
+		public static string RemoveLastChar(this System.String o)
+		{
+			return o.Substring(0, o.Length - 1);
+		}
+	}
+}
