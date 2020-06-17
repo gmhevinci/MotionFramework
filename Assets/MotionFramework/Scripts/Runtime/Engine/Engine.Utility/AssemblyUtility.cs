645da05c9edcb2002b9735e025763a95ee76f818
heguanfeng <heguanfeng@joycastle.mobi>
2020-06-17 14:04:55 +0800

    Update AssemblyUtility.cs
    Fixed performance issues caused by too many assemblies

--- Assets/MotionFramework/Scripts/Runtime/Engine/Engine.Utility/AssemblyUtility.cs
+++ Assets/MotionFramework/Scripts/Runtime/Engine/Engine.Utility/AssemblyUtility.cs
@@ -6,35 +6,62 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Reflection;
 
 namespace MotionFramework.Utility
 {
 	public static class AssemblyUtility
 	{
-		private static readonly List<Type> _cacheTypes = new List<Type>();
+		public const string MotionFrameworkAssemblyName = "MotionFramework";
+		public const string MotionFrameworkAssemblyEditorName = "MotionFramework.Editor";
+		public const string UnityDefaultAssemblyName = "Assembly-CSharp";
+		public const string UnityDefaultAssemblyEditorName = "Assembly-CSharp-Editor";
+
+
+		private static readonly Dictionary<string, List<Type>> _cache = new Dictionary<string, List<Type>>();
 
 		static AssemblyUtility()
 		{
-			_cacheTypes.Clear();
+			_cache.Clear();
+		}
+
+		/// <summary>
+		/// 获取程序集里的所有类型
+		/// </summary>
+		private static List<Type> GetTypes(string assemblyName)
+		{
+			if (_cache.ContainsKey(assemblyName))
+				return _cache[assemblyName];
 
 			Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
 			foreach (Assembly assembly in assemblies)
 			{
-				_cacheTypes.AddRange(assembly.GetTypes());
+				if (assembly.GetName().Name == assemblyName)
+				{
+					List<Type> types = assembly.GetTypes().ToList();
+					_cache.Add(assemblyName, types);
+					return types;
+				}
 			}
+
+			// 注意：如果没有找到程序集返回空列表
+			UnityEngine.Debug.LogWarning($"Not found assembly : {assemblyName}");
+			return new List<Type>();
 		}
 
 		/// <summary>
 		/// 获取带继承关系的所有类的类型
 		/// <param name="parentType">父类类型</param> 
 		/// </summary>
-		public static List<Type> GetAssignableTypes(System.Type parentType)
+		public static List<Type> GetAssignableTypes(string assemblyName, System.Type parentType)
 		{
 			List<Type> result = new List<Type>();
-			for (int i = 0; i < _cacheTypes.Count; i++)
+			List<Type> cacheTypes =	GetTypes(assemblyName);
+			for (int i = 0; i < cacheTypes.Count; i++)
 			{
-				Type type = _cacheTypes[i];
+				Type type = cacheTypes[i];
 
 				// 判断继承关系
 				if (parentType.IsAssignableFrom(type))
@@ -51,12 +78,13 @@ namespace MotionFramework.Utility
 		/// 获取带属性标签的所有类的类型
 		/// <param name="attributeType">属性类型</param>
 		/// </summary>
-		public static List<Type> GetAttributeTypes(System.Type attributeType)
+		public static List<Type> GetAttributeTypes(string assemblyName, System.Type attributeType)
 		{
 			List<Type> result = new List<Type>();
-			for (int i = 0; i < _cacheTypes.Count; i++)
+			List<Type> cacheTypes = GetTypes(assemblyName);
+			for (int i = 0; i < cacheTypes.Count; i++)
 			{
-				System.Type type = _cacheTypes[i];
+				System.Type type = cacheTypes[i];
 
 				// 判断属性标签
 				if (Attribute.IsDefined(type, attributeType))
@@ -72,13 +100,13 @@ namespace MotionFramework.Utility
 		/// </summary>
 		/// <param name="parentType">父类类型</param>
 		/// <param name="attributeType">属性类型</param>
-		/// <returns></returns>
-		public static List<Type> GetAssignableAttributeTypes(System.Type parentType, System.Type attributeType, bool checkError = true)
+		public static List<Type> GetAssignableAttributeTypes(string assemblyName, System.Type parentType, System.Type attributeType, bool checkError = true)
 		{
 			List<Type> result = new List<Type>();
-			for (int i = 0; i < _cacheTypes.Count; i++)
+			List<Type> cacheTypes = GetTypes(assemblyName);
+			for (int i = 0; i < cacheTypes.Count; i++)
 			{
-				Type type = _cacheTypes[i];
+				Type type = cacheTypes[i];
 
 				// 判断属性标签
 				if (Attribute.IsDefined(type, attributeType))
