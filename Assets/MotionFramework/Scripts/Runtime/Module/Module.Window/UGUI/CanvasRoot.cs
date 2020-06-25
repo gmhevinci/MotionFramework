38f4f18797515abadaee4222fa1a9eda7b03689b
何冠峰 <hevinci@hotmail.com>
2020-06-25 11:55:01 +0800

    Update window module
    Support notch screen phone

--- Assets/MotionFramework/Scripts/Runtime/Module/Module.Window/UGUI/CanvasRoot.cs
+++ Assets/MotionFramework/Scripts/Runtime/Module/Module.Window/UGUI/CanvasRoot.cs
@@ -18,6 +18,26 @@ public class CanvasRoot : UIRoot
 	/// </summary>
 	public override Camera UICamera { protected set; get; }
 
+	/// <summary>
+	/// 异形屏支持
+	/// </summary>
+	/// <param name="landscape">是否是横屏</param>
+	/// <param name="offset">偏移值</param>
+	public void NotchSupport(bool landscape, float offset)
+	{
+		var rectTrans = this.UIDesktop.transform as RectTransform;
+		if (landscape)
+		{
+			rectTrans.offsetMin = new Vector2(offset, 0);
+			rectTrans.offsetMax = new Vector2(-offset, 0);
+		}
+		else
+		{
+			rectTrans.offsetMin = new Vector2(0, offset);
+			rectTrans.offsetMax = new Vector2(0, -offset);
+		}
+	}
+
 	protected override void OnAssetLoad(GameObject go)
 	{
 		UIDesktop = Go.transform.BFSearch("UIDesktop").gameObject;
