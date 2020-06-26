68473644ee6ed692f97ee95572121ef6bfd98ed9
何冠峰 <hevinci@hotmail.com>
2020-06-26 15:59:16 +0800

    Update engine core
    Add IActivetorServices.cs file

---
+++ Assets/MotionFramework/Scripts/Runtime/Engine/Core/IActivatorServices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MotionFramework
+{
+	public interface IActivatorServices
+	{
+		/// <summary>
+		/// 创建实例
+		/// </summary>
+		object CreateInstance(Type classType);
+
+		/// <summary>
+		/// 获取特性
+		/// </summary>
+		Attribute GetAttribute(Type classType);
+	}
+}
