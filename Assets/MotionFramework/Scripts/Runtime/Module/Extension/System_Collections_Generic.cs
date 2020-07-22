bf5b05134b24d4b56732153e2ca3927dcee7d953
heguanfeng <heguanfeng@joycastle.mobi>
2020-07-22 16:58:02 +0800

    Update extension

---
+++ Assets/MotionFramework/Scripts/Runtime/Module/Extension/System_Collections_Generic.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+public static class System_Collections_Generic
+{
+	public static bool IsNullOrEmpty<T>(this ICollection<T> collection)
+	{
+		return collection == null || collection.Count == 0;
+	}
+}
