29804a5119ad94ab6a4b23c685304ac41ebae0e8
heguanfeng <heguanfeng@joycastle.mobi>
2020-08-13 14:43:51 +0800

    Update Utility
    Add CRC32

--- Assets/MotionFramework/Scripts/Runtime/Engine/Engine.Utility/HashUtility.cs
+++ Assets/MotionFramework/Scripts/Runtime/Engine/Engine.Utility/HashUtility.cs
@@ -51,11 +51,11 @@ namespace MotionFramework.Utility
 		/// <summary>
 		/// 获取数据流的Hash值
 		/// </summary>
-		public static string StreamSHA1(Stream fs)
+		public static string StreamSHA1(Stream stream)
 		{
 			// 说明：创建的是SHA1类的实例，生成的是160位的散列码
 			HashAlgorithm hash = HashAlgorithm.Create();
-			byte[] hashBytes = hash.ComputeHash(fs);
+			byte[] hashBytes = hash.ComputeHash(stream);
 			return ToString(hashBytes);
 		}
 
@@ -73,7 +73,16 @@ namespace MotionFramework.Utility
 
 		#region MD5
 		/// <summary>
-		/// 获取文件的MD5值
+		/// 获取字符串的MD5
+		/// </summary>
+		public static string StringMD5(string str)
+		{
+			byte[] buffer = Encoding.UTF8.GetBytes(str);
+			return BytesMD5(buffer);
+		}
+
+		/// <summary>
+		/// 获取文件的MD5
 		/// </summary>
 		public static string FileMD5(string filePath)
 		{
@@ -92,7 +101,7 @@ namespace MotionFramework.Utility
 		}
 
 		/// <summary>
-		/// 获取数据流的MD5值
+		/// 获取数据流的MD5
 		/// </summary>
 		public static string StreamMD5(Stream stream)
 		{
@@ -102,13 +111,61 @@ namespace MotionFramework.Utility
 		}
 
 		/// <summary>
-		/// 获取字节数组的MD5值
+		/// 获取字节数组的MD5
 		/// </summary>
 		public static string BytesMD5(byte[] buffer)
 		{
 			MD5CryptoServiceProvider provider = new MD5CryptoServiceProvider();
 			byte[] hashBytes = provider.ComputeHash(buffer);
 			return ToString(hashBytes);
+		}
+		#endregion
+
+		#region CRC32
+		/// <summary>
+		/// 获取字符串的CRC32
+		/// </summary>
+		public static uint StringCRC32(string str)
+		{
+			byte[] buffer = Encoding.UTF8.GetBytes(str);
+			return BytesCRC32(buffer);
+		}
+
+		/// <summary>
+		/// 获取文件的CRC32
+		/// </summary>
+		public static uint FileCRC32(string filePath)
+		{
+			try
+			{
+				using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+				{
+					return StreamCRC32(fs);
+				}
+			}
+			catch (Exception e)
+			{
+				MotionLog.Exception(e.ToString());
+				return 0;
+			}
+		}
+
+		/// <summary>
+		/// 获取数据流的CRC32
+		/// </summary>
+		public static uint StreamCRC32(Stream stream)
+		{
+			CRC32Algorithm hash = new CRC32Algorithm();
+			byte[] hashBytes = hash.ComputeHash(stream);
+			return BitConverter.ToUInt32(hashBytes, 0);
+		}
+
+		/// <summary>
+		/// 获取字节数组的CRC32
+		/// </summary>
+		public static uint BytesCRC32(byte[] buffer)
+		{
+			return CRC32Algorithm.Compute(buffer);
 		}
 		#endregion
 	}
