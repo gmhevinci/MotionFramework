3dc2b394ea94b8cef9cbfac46e80f79d22a2dcf1
何冠峰 <hevinci@hotmail.com>
2021-05-26 14:57:59 +0800

    Update utility

--- Assets/MotionFramework/Scripts/Runtime/Engine/Engine.Utility/ProfilerUtility.cs
+++ Assets/MotionFramework/Scripts/Runtime/Engine/Engine.Utility/ProfilerUtility.cs
@@ -11,31 +11,31 @@ namespace MotionFramework.Utility
 	public static class ProfilerUtility
 	{
 		private static string _watchName;
-		private static long _refMilliseconds;
+		private static long _limitMilliseconds;
 		private static Stopwatch _watch;
 
 		/// <summary>
 		/// 开启性能测试
 		/// </summary>
 		/// <param name="name">测试名称</param>
-		/// <param name="refMilliseconds">推荐的最小毫秒数</param>
+		/// <param name="limitMilliseconds">极限毫秒数</param>
 		[Conditional("DEBUG")]
-		public static void BeginWatch(string name, long refMilliseconds = long.MaxValue)
+		public static void BeginWatch(string name, long limitMilliseconds = long.MaxValue)
 		{
 			if (_watch != null)
 			{
-				UnityEngine.Debug.LogError($"Last watch is not over : {_watchName}");
+				UnityEngine.Debug.LogError($"Last watch is not end : {_watchName}");
 			}
 
 			_watchName = name;
-			_refMilliseconds = refMilliseconds;
+			_limitMilliseconds = limitMilliseconds;
 			_watch = new Stopwatch();
 			_watch.Start();
 		}
 
 		/// <summary>
 		/// 结束性能测试
-		/// 说明：当耗费的毫秒数超过推荐值则输出为警告
+		/// 说明：当耗费的毫秒数超过极限值则输出为警告
 		/// </summary>
 		[Conditional("DEBUG")]
 		public static void EndWatch()
@@ -43,11 +43,11 @@ namespace MotionFramework.Utility
 			if (_watch != null)
 			{
 				_watch.Stop();
-				string logInfo = $"{_watchName} took {_watch.ElapsedMilliseconds} ms";
-				if (_watch.ElapsedMilliseconds > _refMilliseconds)
-					UnityEngine.Debug.Log(logInfo);
-				else
+				string logInfo = $"[Profiler] {_watchName} took {_watch.ElapsedMilliseconds} ms";
+				if (_watch.ElapsedMilliseconds > _limitMilliseconds)
 					UnityEngine.Debug.LogWarning(logInfo);
+				else
+					UnityEngine.Debug.Log(logInfo);
 				_watch = null;
 			}
 		}
