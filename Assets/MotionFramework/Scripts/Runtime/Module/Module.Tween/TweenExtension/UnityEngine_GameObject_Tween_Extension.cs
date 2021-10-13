c4214a309ae82deb48b866938d4d73d2fe722634
hevinci <hevinci@hotmail.com>
2021-10-13 14:15:23 +0800

    Update tween module

---
+++ Assets/MotionFramework/Scripts/Runtime/Module/Module.Tween/TweenExtension/UnityEngine_GameObject_Tween_Extension.cs
@@ -0,0 +1,21 @@
+﻿//--------------------------------------------------
+// Motion Framework
+// Copyright©2020-2020 何冠峰
+// Licensed under the MIT license
+//--------------------------------------------------
+using MotionFramework.Tween;
+
+namespace UnityEngine
+{
+    public static class UnityEngine_GameObject_Tween_Extension
+    {
+        public static long PlayTween(this GameObject go, ITweenNode tween)
+        {
+            return TweenManager.Instance.Play(tween, go);
+        }
+        public static long PlayTween(this GameObject go, ITweenChain tween)
+        {
+            return TweenManager.Instance.Play(tween, go);
+        }
+    }
+}
