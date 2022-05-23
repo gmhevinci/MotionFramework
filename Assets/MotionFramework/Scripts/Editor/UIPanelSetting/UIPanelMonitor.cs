2920c315413107cbdb0d3e9537af31f679492d32
hevinci <hevinci@hotmail.com>
2022-05-23 22:35:50 +0800

    Update UIPanelMonitor.cs

--- Assets/MotionFramework/Scripts/Editor/UIPanelSetting/UIPanelMonitor.cs
+++ Assets/MotionFramework/Scripts/Editor/UIPanelSetting/UIPanelMonitor.cs
@@ -8,9 +8,7 @@ using UnityEditor;
 
 #if UNITY_2021_2_OR_NEWER
 using UnityEditor.SceneManagement;
-#endif
-
-#if UNITY_2018_4 || UNITY_2019_4 || UNITY_2020_3
+#else
 using UnityEditor.Experimental.SceneManagement;
 #endif
 
@@ -21,20 +19,9 @@ namespace MotionFramework.Editor
 		[InitializeOnLoadMethod]
 		static void StartInitializeOnLoadMethod()
 		{
-#if UNITY_2018_4_OR_NEWER
 			PrefabStage.prefabSaving += OnPrefabSaving;
-#else
-			// 监听Inspector的Apply事件
-			PrefabUtility.prefabInstanceUpdated = delegate (GameObject go)
-			{
-				UIManifest manifest = go.GetComponent<UIManifest>();
-				if (manifest != null)
-					UIPanelModifier.Refresh(manifest);
-			};
-#endif
 		}
 
-#if UNITY_2018_4_OR_NEWER
 		static void OnPrefabSaving(GameObject go)
 		{
 			PrefabStage stage = PrefabStageUtility.GetCurrentPrefabStage();
@@ -45,6 +32,5 @@ namespace MotionFramework.Editor
 					UIPanelModifier.Refresh(manifest);
 			}
 		}
-#endif
 	}
 }
